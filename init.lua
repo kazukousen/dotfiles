46761710f2a83337ffdc461277743b15da7007d1
nitta-kazuki <mmchari.0228@gmail.com>
2020-05-09 11:10:56 +0900

    move an application window between displays

--- init.lua
+++ init.lua
@@ -1,6 +1,9 @@
+-- Hello World
 hs.hotkey.bind({"cmd", "alt", "ctrl"}, "W", function()
   hs.alert.show("Hello World!")
 end)
+
+-- ctrl hjkl
 
 local function keyCode(key, modifiers)
    modifiers = modifiers or {}
@@ -25,15 +28,37 @@ remapKey({'ctrl'}, 'l', keyCode('right'))
 local quitModal = hs.hotkey.modal.new('cmd','q')
 
 function quitModal:entered()
-    hs.alert.show("Press Cmd+Q again to quit", 1)
-    hs.timer.doAfter(1, function() quitModal:exit() end)
+   hs.alert.show("Press Cmd+Q again to quit", 1)
+   hs.timer.doAfter(1, function() quitModal:exit() end)
 end
 
 local function doQuit()
-    local res = hs.application.frontmostApplication():selectMenuItem("^Quit.*$")
-    quitModal:exit()
+   local res = hs.application.frontmostApplication():selectMenuItem("^Quit.*$")
+   quitModal:exit()
 end
 
 quitModal:bind('cmd', 'q', doQuit)
+
+-- Move application between displays
+
+function moveWindowToDisplay(d)
+   return function()
+      local displays = hs.screen.allScreens()
+      local win = hs.window.focusedWindow()
+      local wasFullScreen = win:isFullScreen()
+      if wasFullScreen then
+         win:toggleFullScreen()
+      end
+      win:moveToScreen(displays[d], false, true)
+      if wasFullScreen then
+         win:toggleFullScreen()
+      end
+   end
+end
+
+hs.hotkey.bind({"option", "shift"}, "1", moveWindowToDisplay(1))
+hs.hotkey.bind({"option", "shift"}, "2", moveWindowToDisplay(2))
+
+--
 
 quitModal:bind('', 'escape', function() quitModal:exit() end)
